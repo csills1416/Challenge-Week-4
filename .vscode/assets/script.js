a413231e8f9ca4d428cbf6300e1deadb8411f24b
CSillus <71670415+csills1416@users.noreply.github.com>
2023-06-10 15:55:16 -0400

    added initials, highscore var, used '' instead of "", removed unnecssary java.

--- .vscode/assets/script.js
+++ .vscode/assets/script.js
@@ -6,50 +6,53 @@ var timerElement = document.getElementById('time');
 var gameOverElement = document.getElementById('end-screen');
 var finalScoreElement = document.getElementById('final-score');
 var highScoresElement = document.getElementById('high-scores-list');
+var initialsForm = document.getElementById('initials-form');
+var initialsInput = document.getElementById('initials');
 
 var currentQuestionIndex = 0;
 var time = 0;
 var timerId;
+var highScores = [];
 
 var questions = [
-  {
-    question: "What are strings?",
-    solution: [
-      "Text", 
-      "Used for storing and manipulating text", 
-      "A Piece of Fabric", 
-      "None of the above",],
-    answer: "Used for storing and manipulating text"
-  },
-  {
-    question: "What is a function?",
-    solution: [
-      "Function", 
-      "Used for creating functions", 
-      "A Method", 
-      "None of the above",],
-    answer: "Used for creating functions"
-  },
-  {
-    question: "What is a class?",
-    solution: [
-      "Class", 
-      "A Class", 
-      "A Constructor", 
-      "None of the above", 
-      "Used for creating classes"],
-    answer: "Used for creating classes"
-  },
-  {
-    question: "What is an object?",
-    solution: [
-      "Used for creating objects",
-      "Phsycal Attribute", 
-      "A Property",
-      "None of the above", ],
-    answer: "Used for creating objects"
-  }
-];
+    {
+      question: "What are strings?",
+      solution: [
+        "Text", 
+        "Used for storing and manipulating text", 
+        "A Piece of Fabric", 
+        "None of the above",],
+      answer: "Used for storing and manipulating text"
+    },
+    {
+      question: "What is a function?",
+      solution: [
+        "Function", 
+        "Used for creating functions", 
+        "A Method", 
+        "None of the above",],
+      answer: "Used for creating functions"
+    },
+    {
+      question: "What is a class?",
+      solution: [
+        "Class", 
+        "A Class", 
+        "A Constructor", 
+        "None of the above", 
+        "Used for creating classes"],
+      answer: "Used for creating classes"
+    },
+    {
+      question: "What is an object?",
+      solution: [
+        "Used for creating objects",
+        "Phsycal Attribute", 
+        "A Property",
+        "None of the above", ],
+      answer: "Used for creating objects"
+    }
+  ];
 
 function startQuiz() {
   startButton.classList.add('hidden');
@@ -63,13 +66,13 @@ function startQuiz() {
 function showQuestion() {
   var currentQuestion = questions[currentQuestionIndex];
   questionElement.textContent = currentQuestion.question;
-  solutionElement.innerHTML = "";
+  solutionElement.innerHTML = '';
 
   currentQuestion.solution.forEach(function (solution) {
-    var button = document.createElement("button");
+    var button = document.createElement('button');
     button.textContent = solution;
     button.classList.add('solution');
-    button.addEventListener("click", handleSolution);
+    button.addEventListener('click', handleSolution);
     solutionElement.appendChild(button);
   });
 }
@@ -104,48 +107,56 @@ function endQuiz() {
   questionContainer.classList.add('hidden');
   gameOverElement.classList.remove('hidden');
   finalScoreElement.textContent = time;
+  saveScore();
+  updateHighScoresList();
 }
 
-startButton.addEventListener('click', startQuiz);
-
-document.getElementById('intials-form').addEventListener('submit', function (event) {
-  event.preventDefault();
+function saveScore() {
   var initials = initialsInput.value.trim();
-  if (initials !== "") {
-    saveScore(initials, time);
-    initialsForm.reset();
+  if (initials !== '') {
+    var newScore = {
+      initials: initials,
+      score: time
+    };
+    highScores.push(newScore);
+    highScores.sort(function (a, b) {
+      return b.score - a.score;
+    });
+    if (highScores.length > 10) {
+      highScores.pop();
+    }
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    document.getElementById('final-score').textContent = time;
+    initialsInput.value = '';
   }
-});
-
-function saveScore(initials, time) {
-  var highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-  var newScore = {
-    initials: initials,
-    score: time
-  };
-  highScores.push(newScore);
-  highScores.sort(function (a, b) {
-    return b.score - a.score;
-  });
-  if (highScores.length > 10) {
-    highScores.pop();
-  }
-  localStorage.setItem('highScores', JSON.stringify(highScores));
-  document.getElementById('final-score').textContent = time;
-  initialsInput.value = '';
-
- 
 }
-updateHighScoresList(highScores);
 
-function updateHighScoresList(highScores) {
-  highScoresList.innerHTML = '';
+function updateHighScoresList() {
+  highScoresElement.innerHTML = '';
 
   highScores.forEach(function (score) {
     var listItem = document.createElement('li');
-    listItem.textContent = score.initials + ' - '+ score.score;
-    highScoresList.appendChild(listItem);
+    listItem.textContent = score.initials + ' - ' + score.score;
+    highScoresElement.appendChild(listItem);
   });
 }
 
 startButton.addEventListener('click', startQuiz);
+initialsForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var intials = initialsInput.value.trim();
+    saveScore();
+    initialsForm.reset();
+    updateHighScoresList();
+  });
+
+  function loadHighScores() {
+    var savedHighScores = JSON.parse(localStorage.getItem('highScores'));
+    if (savedHighScores!== null) {
+      highScores = savedHighScores;
+      updateHighScoresList();
+    }
+  }
+
+  loadHighScores();
+
